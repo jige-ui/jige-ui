e2df0f6ac7ba2756450ed2b92e646607ff446238
g-mero <gmero@foxmail.com>
2025-02-05 08:57:41 +0800

    feat: add component 'segment'

---
+++ apps/docs/src/pages/components/segment/Demo.tsx
@@ -0,0 +1,27 @@
+import { Segment } from 'jige-ui'
+import { createSignal } from 'solid-js'
+import { createStore } from 'solid-js/store'
+import { Playground } from '~/components/playground'
+
+export function Demo() {
+  const [value, setValue] = createSignal('1')
+  const [s, setS] = createStore({
+    disabled: false,
+  })
+  return (
+    <Playground>
+      <Playground.MainArea>
+        <div>
+          <div class="b b-t-border rounded-md mb-2">
+            <div class="p-2">
+              Selected value:
+              {value()}
+            </div>
+          </div>
+          <Segment value={value()} onChange={setValue} options={['1', '2', '3']} disabled={s.disabled} />
+        </div>
+      </Playground.MainArea>
+      <Playground.PropertySetting properties={s} onChange={setS} />
+    </Playground>
+  )
+}
