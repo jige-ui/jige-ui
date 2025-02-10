fdcbc3c54507fbe5821529ad2e64e781f8df815e
g-mero <gmero@foxmail.com>
2025-02-10 09:41:38 +0800

    feat: add component 'number-box'

---
+++ packages/jige-ui/src/components/number-box/context.ts
@@ -0,0 +1,25 @@
+import { isNumber } from 'radash'
+import { createComponentState } from 'solid-uses'
+
+export const context = createComponentState({
+  state: () => ({
+    value: Number.NaN,
+    disabled: false,
+    min: Number.NEGATIVE_INFINITY,
+    max: Number.POSITIVE_INFINITY,
+    name: 'number-box',
+    focused: false,
+  }),
+  methods: {
+    setValue(value: number) {
+      if (value > this.state.max) {
+        this.actions.setState('value', this.state.max)
+      } else if (value < this.state.min) {
+        this.actions.setState('value', this.state.min)
+      }
+      else {
+        this.actions.setState('value', value)
+      }
+    }
+  }
+})
