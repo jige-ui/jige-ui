277f66f885396183c068bc2c3fd8e8499327626b
g-mero <gmero@foxmail.com>
2025-02-14 16:04:39 +0800

    chore(docs): optimized playground

--- apps/docs/src/components/playground/PropertySetting.tsx
+++ apps/docs/src/components/playground/PropertySetting.tsx
@@ -1,14 +1,18 @@
 import type { InitialValue } from 'jige-core'
 import type { SetStoreFunction } from 'solid-js/store'
 import { FormCore } from 'jige-core'
-import { Input, NumberBox, Switcher } from 'jige-ui'
+import { ComboBox, Input, NumberBox, Switcher } from 'jige-ui'
 import { createMemo, For, Match, Switch } from 'solid-js'
 
 export function PropertySetting<T extends InitialValue>(props: {
   properties: T
   onChange: SetStoreFunction<T>
+  typeDeclaration?: Record<string, 'string' | 'number' | 'boolean' | string[]>
 }) {
   const propNames = createMemo(() => Object.keys(props.properties))
+  const checkType = (key: string) => {
+    return props.typeDeclaration?.[key] || typeof props.properties[key]
+  }
   return (
     <div class="flex flex-col w-185px">
       <div class="b-b p-2 b-t-border">
@@ -19,22 +23,28 @@ export function PropertySetting<T extends InitialValue>(props: {
           {item => (
             <FormCore.Item name={item}>
               <Switch>
-                <Match when={typeof props.properties[item] === 'boolean'}>
+                <Match when={checkType(item) === 'boolean'}>
                   <div class="flex justify-between items-center m-1">
                     <label>{item}</label>
                     <Switcher type="checkbox" />
                   </div>
                 </Match>
-                <Match when={typeof props.properties[item] === 'string'}>
-                  <div>
+                <Match when={checkType(item) === 'string'}>
+                  <div class="flex justify-between items-center m-1">
                     <label>{item}</label>
                     <Input />
                   </div>
                 </Match>
-                <Match when={typeof props.properties[item] === 'number'}>
-                  <div>
+                <Match when={checkType(item) === 'number'}>
+                  <div class="flex justify-between items-center m-1">
                     <label>{item}</label>
                     <NumberBox />
+                  </div>
+                </Match>
+                <Match when={Array.isArray(checkType(item))}>
+                  <div class="flex justify-between items-center m-1">
+                    <label>{item}</label>
+                    <ComboBox options={checkType(item) as string[]} />
                   </div>
                 </Match>
               </Switch>
