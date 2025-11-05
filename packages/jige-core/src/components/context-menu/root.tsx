0d96c8dd73e54d9aa75e13a4c1c14407b6eb1ead
g-mero <gmerocom@gmail.com>
2025-11-05 15:06:26 +0800

    chore: remove context-menu

--- packages/jige-core/src/components/context-menu/root.tsx
+++
@@ -1,8 +0,0 @@
-import type { JSX } from "solid-js/jsx-runtime";
-import context from "./context";
-
-export function Root(props: { children: JSX.Element }) {
-  const Context = context.initial();
-
-  return <Context.Provider>{props.children}</Context.Provider>;
-}
